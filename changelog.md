a242698fd107a83b645188a8ad6ac4dda7b1fbff
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2026-02-22 10:57:50 -0500

    derp

--- changelog.md
+++ changelog.md
@@ -1,3 +1,3 @@
 # DropSimetricoPreparate Changelog
-## v1.0.0
+## v1.0.1
 - Initial release (on GitHub).
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include "geode.custom-keybinds/include/Keybinds.hpp"
 #include <Geode/modify/EditorUI.hpp>
 #include <Geode/modify/UILayer.hpp>
 
@@ -6,17 +5,29 @@ using namespace geode::prelude;
 using namespace keybinds;
 
 class $modify(MyUILayer, UILayer) {
-	void defineKeybind(const char* id, const std::function<ListenerResult(bool)>& callback) const {
-		PlayLayer* pl = PlayLayer::get();
-		if (!pl) return;
-		pl->addEventListener<InvokeBindFilter>([this, callback](const InvokeBindEvent* event) {
-			return callback(event->isDown());
-		}, id);
-	}
 	bool init(GJBaseGameLayer* gjbgl) {
 		if (!UILayer::init(gjbgl)) return false;
 		Loader::get()->queueInMainThread([this] {
 			if (!PlayLayer::get()) return;
+			this->addEventListener(
+				KeybindSettingPressedEventV3(Mod::get(), "both-players-jump"),
+				[this](Keybind const& keybind, bool down, bool repeat, double timestamp) {
+					this->queueButtonPL(static_cast<int>(PlayerButton::Jump), down, timestamp);
+				}
+			);
+			this->addEventListener(
+				KeybindSettingPressedEventV3(Mod::get(), "both-players-left"),
+				[this](Keybind const& keybind, bool down, bool repeat, double timestamp) {
+					this->queueButtonPL(static_cast<int>(PlayerButton::Left), down, timestamp);
+				}
+			);
+			this->addEventListener(
+				KeybindSettingPressedEventV3(Mod::get(), "both-players-right"),
+				[this](Keybind const& keybind, bool down, bool repeat, double timestamp) {
+					this->queueButtonPL(static_cast<int>(PlayerButton::Right), down, timestamp);
+				}
+			);
+			/*
 			this->defineKeybind("both-players-jump"_spr, [this](const bool down) {
 				this->queueButtonPL(static_cast<int>(PlayerButton::Jump), down);
 				return ListenerResult::Propagate;
@@ -29,6 +40,7 @@ class $modify(MyUILayer, UILayer) {
 				this->queueButtonPL(static_cast<int>(PlayerButton::Right), down);
 				return ListenerResult::Propagate;
 			});
+			*/
 		});
 		return true;
 	}
@@ -42,28 +54,41 @@ class $modify(MyUILayer, UILayer) {
 		const auto playLayer = CCScene::get()->getChildByType<PlayLayer>(0);
 		return playLayer != nullptr && playLayer == PlayLayer::get() && playLayer->getChildByType<UILayer>(0) == this;
 	}
-	void queueButtonPL(const int buttonNumber, const bool isDown) const {
+	void queueButtonPL(const int buttonNumber, const bool isDown, const double timestamp) const {
 		PlayLayer* pl = PlayLayer::get();
 		if (!pl || this->isPaused()) return;
 		if (GJGameLevel* level = pl->m_level) {
 			if (!level->m_twoPlayerMode) return;
 			if (buttonNumber != 1 && !level->isPlatformer()) return;
 		}
-		pl->queueButton(buttonNumber, isDown, false);
-		pl->queueButton(buttonNumber, isDown, true);
+		pl->queueButton(buttonNumber, isDown, false, timestamp);
+		pl->queueButton(buttonNumber, isDown, true, timestamp);
 	}
 };
 
 class $modify(MyEditorUI, EditorUI) {
-	void defineKeybind(const std::string& id, const std::function<void(bool)> &callback) {
-		this->addEventListener<InvokeBindFilter>([=](const InvokeBindEvent* event) {
-			callback(event->isDown());
-			return ListenerResult::Propagate;
-		}, id);
-	}
 	bool init(LevelEditorLayer* lel) {
 		if (!EditorUI::init(lel)) return false;
 		Loader::get()->queueInMainThread([this] {
+			this->addEventListener(
+				KeybindSettingPressedEventV3(Mod::get(), "both-players-jump"),
+				[this](Keybind const& keybind, bool down, bool repeat, double timestamp) {
+					MyEditorUI::queueButtonLEL(static_cast<int>(PlayerButton::Jump), down, timestamp);
+				}
+			);
+			this->addEventListener(
+				KeybindSettingPressedEventV3(Mod::get(), "both-players-left"),
+				[this](Keybind const& keybind, bool down, bool repeat, double timestamp) {
+					MyEditorUI::queueButtonLEL(static_cast<int>(PlayerButton::Left), down, timestamp);
+				}
+			);
+			this->addEventListener(
+				KeybindSettingPressedEventV3(Mod::get(), "both-players-right"),
+				[this](Keybind const& keybind, bool down, bool repeat, double timestamp) {
+					MyEditorUI::queueButtonLEL(static_cast<int>(PlayerButton::Right), down, timestamp);
+				}
+			);
+			/*
 			this->defineKeybind("both-players-jump"_spr, [](const bool down) {
 				MyEditorUI::queueButtonLEL(static_cast<int>(PlayerButton::Jump), down);
 			});
@@ -73,42 +98,18 @@ class $modify(MyEditorUI, EditorUI) {
 			this->defineKeybind("both-players-right"_spr, [](const bool down) {
 				MyEditorUI::queueButtonLEL(static_cast<int>(PlayerButton::Right), down);
 			});
+			*/
 		});
 		return true;
 	}
-	static void queueButtonLEL(const int buttonNumber, const bool isDown) {
+	static void queueButtonLEL(const int buttonNumber, const bool isDown, const double timestamp) {
 		LevelEditorLayer* lel = LevelEditorLayer::get();
 		if (!lel || lel->m_playbackMode != PlaybackMode::Playing) return;
 		if (GJGameLevel* level = lel->m_level) {
 			if (!level->m_twoPlayerMode) return;
 			if (buttonNumber != 1 && !level->isPlatformer()) return;
 		}
-		lel->queueButton(buttonNumber, isDown, false);
-		lel->queueButton(buttonNumber, isDown, true);
+		lel->queueButton(buttonNumber, isDown, false, timestamp);
+		lel->queueButton(buttonNumber, isDown, true, timestamp);
 	}
 };
-
-$on_mod(Loaded) {
-	BindManager* binds = BindManager::get();
-	binds->registerBindable({
-		"both-players-jump"_spr,
-		"Both Players Jump",
-		"Make both players jump.",
-		{ Keybind::create(KEY_I) },
-		"Play/DropSimetricoPreparate", false
-	});
-	binds->registerBindable({
-		"both-players-left"_spr,
-		"Both Players Left",
-		"Make both players move to the left.",
-		{ Keybind::create(KEY_J) },
-		"Play/DropSimetricoPreparate", false
-	});
-	binds->registerBindable({
-		"both-players-right"_spr,
-		"Both Players Right",
-		"Make both players move to the right.",
-		{ Keybind::create(KEY_L) },
-		"Play/DropSimetricoPreparate", false
-	});
-}
